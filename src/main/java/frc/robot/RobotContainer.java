f7d8ace52ef1e68e0fb6b38084801cb9582bea77
Lawrence Welty <78815772+WeltyLawrence@users.noreply.github.com>
2024-01-23 20:09:52 -0500

    remove unused reference for shuffleboard

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,7 +5,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import frc.robot.commands.DriveMecanumFieldCentric;
